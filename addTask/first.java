95bebb3ea6a5208dc0fbd3ff60588b4660f238d6
San4izess <san4izess@gmail.com>
2023-03-29 20:01:04 +0300

    update first

--- addTask/first.java
+++ addTask/first.java
@@ -1,9 +1,5 @@
 package addTask;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class first {
